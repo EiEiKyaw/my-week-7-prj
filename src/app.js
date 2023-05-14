5084793dd9994b40aefcb99db3d9fe7c33173bd2
ei_ei_kyaw_97 <39520950+EiEiKyaw@users.noreply.github.com>
2023-05-14 23:38:32 +0630

    add the current weather search engine

---
+++ src/app.js
@@ -0,0 +1,96 @@
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let day = formatDay(timestamp);
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayCurrentWeather(response) {
+  let data = response.data;
+  currentTemp = response.data.temperature.current;
+  console.log(data);
+  let cityEle = document.querySelector("#city-name");
+  let countryEle = document.querySelector("#country-name");
+  let dateEle = document.querySelector("#date");
+  let descEle = document.querySelector("#description");
+  let iconEle = document.querySelector("#weather-icon");
+  let humidityEle = document.querySelector("#humidity-value");
+  let windEle = document.querySelector("#wind-value");
+  let pressureEle = document.querySelector("#pressure-value");
+  let feelsEle = document.querySelector("#fl-value");
+
+  cityEle.innerHTML = data.city;
+  countryEle.innerHTML = data.country;
+  dateEle.innerHTML = formatDate(data.time * 1000);
+  descEle.innerHTML = data.condition.description;
+  iconEle.setAttribute("src", data.condition.icon_url);
+  tempEle.innerHTML = Math.round(currentTemp);
+  humidityEle.innerHTML = Math.round(data.temperature.humidity);
+  windEle.innerHTML = Math.round(data.wind.speed);
+  pressureEle.innerHTML = Math.round(data.temperature.pressure);
+  feelsEle.innerHTML = Math.round(data.temperature.feels_like);
+}
+
+function searchWeather(cityName) {
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityName}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayCurrentWeather);
+}
+
+function processSearching(event) {
+  event.preventDefault();
+  let searchCity = document.querySelector("#search-city");
+  searchWeather(searchCity.value);
+}
+
+function convertFahrenheit(event) {
+  event.preventDefault();
+  celEle.classList.remove("active");
+  fahEle.classList.add("active");
+  let fahTemp = (currentTemp * 9) / 5 + 32;
+  tempEle.innerHTML = Math.round(fahTemp);
+}
+
+function convertCelcius(event) {
+  event.preventDefault();
+  fahEle.classList.remove("active");
+  celEle.classList.add("active");
+  tempEle.innerHTML = Math.round(currentTemp);
+}
+
+function callCurrentPosition(position) {
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${long}&lat=${lat}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayCurrentWeather);
+}
+
+let apiKey = "3tf4404efa0b2d41d89ob6d7a3f07dbc";
+let form = document.querySelector("#search-form");
+let currentTemp = 0;
+let tempEle = document.querySelector("#temperature");
+let fahEle = document.querySelector("#fahrenheit-unit");
+let celEle = document.querySelector("#celcius-unit");
+
+form.addEventListener("submit", processSearching);
+
+fahEle.addEventListener("click", convertFahrenheit);
+
+celEle.addEventListener("click", convertCelcius);
+
+navigator.geolocation.getCurrentPosition(callCurrentPosition);
